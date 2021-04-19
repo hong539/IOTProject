9787df2542c98d6077c5f23a19965a6ba1a57299
bweng3310 <82631286+bweng3310@users.noreply.github.com>
2021-04-19 12:48:23 +0800

    Relay Test Ok

--- Relay/relay.c
+++ Relay/relay.c
@@ -16,39 +16,42 @@ int main(int argc, char **argv)
 
     int value;
     if (!bcm2835_init())
-  return 1;
+	return 1;
 
     // Set the pin to be an output
     bcm2835_gpio_fsel(RELAY, BCM2835_GPIO_FSEL_OUTP);
-    bcm2835_gpio_fsel(LED2, BCM2835_GPIO_FSEL_OUTP);
-    bcm2835_gpio_fsel(DHT, BCM2835_GPIO_FSEL_INPT);
-  bcm2835_gpio_fsel(COM, BCM2835_GPIO_FSEL_OUTP);
-  bcm2835_gpio_write(COM, HIGH);
+   // bcm2835_gpio_fsel(LED2, BCM2835_GPIO_FSEL_OUTP);
+   // bcm2835_gpio_fsel(BTN1, BCM2835_GPIO_FSEL_INPT);
+	bcm2835_gpio_fsel(COM, BCM2835_GPIO_FSEL_OUTP);
+	bcm2835_gpio_write(COM, HIGH);
     // Blink
-  
+	
     while (1)
     { 
-  humidity = input;
-   //value= bcm2835_gpio_lev(BTN1);
-   printf("read from DHT_INPUT: %d\n", value); 
-    
-  if (input >= 65%) { 
-  // Turn it on
-  bcm2835_gpio_write(RELAY, HIGH);
-  bcm2835_gpio_write(LED2, HIGH);
+		
+	// value= bcm2835_gpio_lev(BTN1);
+	// printf("read from BTN1_INPUT: %d\n", value);	
+		
+	//if (value==1) {	
+	// Turn it on
+	bcm2835_gpio_write(RELAY, HIGH);
+	//bcm2835_gpio_write(LED2, HIGH);
+	
+	bcm2835_delay(3000);
+		
 
-  
-    } else {
-  
-  // turn it off
-  bcm2835_gpio_write(RELAY, LOW);
-  bcm2835_gpio_write(LED2, LOW);
+   // } else {
+	
+	// turn it off
+	bcm2835_gpio_write(RELAY, LOW);
+	bcm2835_delay(3000);
+	//bcm2835_gpio_write(LED2, LOW);
 
 
-    }
-  
-  bcm2835_delay(100);  //500ms
-  }
+   // }
+	
+	bcm2835_delay(100);  //500ms
+	}
     bcm2835_close();
     return 0;
 }
