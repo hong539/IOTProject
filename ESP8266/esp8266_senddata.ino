2a3d9d46ae6ba74343c27f66b0be6f6dc599f52c
wsxedc54545 <50731647+wsxedc54545@users.noreply.github.com>
2021-04-20 22:52:23 +0800

    Add files via upload
    DHT11/MQ-2

---
+++ ESP8266/esp8266_senddata.ino
@@ -0,0 +1,103 @@
+
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <Wire.h>
+#include <DHT.h>
+
+// Replace with your network credentials
+const char* ssid     = "Yuan";
+const char* password = "00000000";
+
+// REPLACE with your Domain name and URL path or IP address with path
+const char* serverName = "http://192.168.43.211/post-esp-data.php";
+
+// Keep this API Key value to be compatible with the PHP code provided in the project page. 
+// If you change the apiKeyValue value, the PHP file /post-esp-data.php also needs to have the same key 
+String apiKeyValue = "tPmAT5Ab3j7F9";
+
+String sensorName = "esp8266-1";
+String sensorLocation = "myroom";
+
+int smokeA0 = D0;
+// Your threshold value
+int sensorThres = 500;
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+#define DHTPIN D1
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN,DHTTYPE,11);  // I2C
+
+
+void setup() {
+  Serial.begin(115200);
+  
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) { 
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  pinMode(smokeA0, INPUT);
+  // (you can also pass in a Wire library object like &Wire2)
+  dht.begin();
+
+}
+
+void loop() {
+  //Check WiFi connection status
+  if(WiFi.status()== WL_CONNECTED){
+    HTTPClient http;
+    
+    // Your Domain name with URL path or IP address with path
+    http.begin(serverName);
+    
+    // Specify content-type header
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    int analogSensor = analogRead(smokeA0);
+    
+    // Prepare your HTTP POST request data
+    String httpRequestData = "api_key=" + apiKeyValue + "&sensor=" + sensorName
+                          + "&location=" + sensorLocation + "&value1=" + String(dht.readTemperature()) +"°C"
+                          + "&value2=" + String(dht.readHumidity()) + "%" + "&value3="  + String(analogRead(smokeA0)) +"ppm";
+    Serial.print("httpRequestData: ");
+    Serial.println(httpRequestData);
+    
+    // You can comment the httpRequestData variable above
+    // then, use the httpRequestData variable below (for testing purposes without the BME280 sensor)
+    //String httpRequestData = "api_key=tPmAT5Ab3j7F9&sensor=BME280&location=Office&value1=24.75&value2=49.54&value3=1005.14";
+
+    // Send HTTP POST request
+    int httpResponseCode = http.POST(httpRequestData);
+     
+    // If you need an HTTP request with a content type: text/plain
+    //http.addHeader("Content-Type", "text/plain");
+    //int httpResponseCode = http.POST("Hello, World!");
+    
+    // If you need an HTTP request with a content type: application/json, use the following:
+    //http.addHeader("Content-Type", "application/json");
+    //int httpResponseCode = http.POST("{\"value1\":\"19\",\"value2\":\"67\",\"value3\":\"78\"}");
+        
+    if (httpResponseCode>0) {
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+    }
+    else {
+      Serial.print("Error code: ");
+      Serial.println(httpResponseCode);
+    }
+    // Free resources
+    http.end();
+  }
+  else {
+    Serial.println("WiFi Disconnected");
+  }
+  //Send an HTTP POST request every 30 seconds
+  delay(30000);  
+}
